2640e5555d7f66ef96acc75053191e67dd93931f
A1cey <mschelkle05@web.de>
2024-11-14 12:41:45 +0100

    Defined Parser Types

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,31 @@
 use crate::error::Error;
+use crate::functions::Func;
 use crate::types::Token;
 
 pub fn parse(tokens: &Vec<Token>) -> Result<String, Error> {
     Ok(format!("{:?}", tokens))
+}
+
+enum ExpressionOption {
+    Number(Number),
+    Variable(Variable),
+    Function(Function),
+}
+
+struct Expression {
+    value: ExpressionOption,
+}
+
+struct Number {
+    number: f64,
+}
+
+struct Variable {
+    name: String,
+    value: Option<f64>,
+}
+
+struct Function {
+    function: Func,
+    params: Vec<Expression>,
 }
